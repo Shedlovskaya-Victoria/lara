ddebd8f4ae6357d23e17508edcbec1a232536431
Shed <dvikashe@gmail.com>
2024-02-09 15:54:31 +1000

    test(tag_seed): add
    new

--- lara/database/seeders/DatabaseSeeder.php
+++ lara/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Category::factory(10)->create();
         \App\Models\Post::factory(100)->create();
         \App\Models\Tag::factory(20)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
